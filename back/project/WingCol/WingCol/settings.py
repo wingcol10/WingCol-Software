cedc8fde96f84156b4f95f60b3e3ac9ce2b0bae5
wingcol10 <wingcolairlines@gmail.com>
2024-10-17 22:05:51 -0500

    ca

--- back/project/WingCol/WingCol/settings.py
+++ back/project/WingCol/WingCol/settings.py
@@ -14,9 +14,6 @@ from pathlib import Path
 from datetime import timedelta
 import environ
 
-import pymysql
-pymysql.install_as_MySQLdb()
-
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
